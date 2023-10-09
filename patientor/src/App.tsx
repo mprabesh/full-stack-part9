46f48cec4aa98b304a4dadc4474c0c9970e9bad7
mprabesh <theprabesh723@gmail.com>
2023-10-10 01:06:37 +0545

    feat:patientor app frontend initialized

---
+++ patientor/src/App.tsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { Button, Divider, Container, Typography } from "@mui/material";
+
+import { Patient } from "./types";
+
+import patientService from "./services/patients";
+import PatientListPage from "./components/PatientListPage";
+
+const App = () => {
+  const [patients, setPatients] = useState<Patient[]>([]);
+
+  useEffect(() => {
+    const fetchPatientList = async () => {
+      const patients = await patientService.getAll();
+      setPatients(patients);
+    };
+    void fetchPatientList();
+  }, []);
+
+  return (
+    <div className="App">
+      <Router>
+        <Container>
+          <Typography variant="h3" style={{ marginBottom: "0.5em" }}>
+            Patientor
+          </Typography>
+          <Button component={Link} to="/" variant="contained" color="primary">
+            Home
+          </Button>
+          <Divider hidden />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PatientListPage
+                  patients={patients}
+                  setPatients={setPatients}
+                />
+              }
+            />
+          </Routes>
+        </Container>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
