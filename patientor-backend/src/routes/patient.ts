34135019e10623026db37a0abafff977e19f3a28
mprabesh <theprabesh723@gmail.com>
2023-10-10 22:32:24 +0545

    feat:patient post request modified with proofing

--- patientor-backend/src/routes/patient.ts
+++ patientor-backend/src/routes/patient.ts
@@ -1,16 +1,24 @@
 import express from "express";
 const patientRouter = express.Router();
 import { getAllPatient, addPatient } from "../services/patient";
+import { toNewPatientEntry } from "../utils";
 
 patientRouter.get("/", (req, res) => {
   res.status(200).json(getAllPatient());
 });
 
 patientRouter.post("/", (req, res) => {
-  /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
-  const newEntry = addPatient({ name, dateOfBirth, ssn, gender, occupation });
-  res.json(newEntry);
+  try {
+    const newPatient = toNewPatientEntry(req.body);
+    const newEntry = addPatient(newPatient);
+    res.status(200).json(newEntry);
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong.";
+    if (error instanceof Error) {
+      errorMessage += " Error: " + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default patientRouter;
